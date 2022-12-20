8b0882290be7755b8c3a1e04dc0726fe71635c18
Almaznur1 <aboohnifa@gmail.com>
2022-12-20 23:49:22 +0300

    step3 completed

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import requests
-import pathlib
+from pathlib import Path
 import os
 
-scripts_dir = os.path.dirname(os.path.abspath(__file__))
-pathlib.Path(f'{scripts_dir}/images').mkdir(parents=True, exist_ok=True)
+
+def get_space_photo(url, output_dir):
+    if os.path.isabs(output_dir):
+        Path(f'{output_dir}').mkdir(parents=True, exist_ok=True)
+        filename = f'{output_dir}/hubble.jpeg'
+    else:
+        scripts_dir = os.path.dirname(os.path.abspath(__file__))
+        Path(f'{scripts_dir}/{output_dir}').mkdir(parents=True, exist_ok=True)
+        filename = f'{scripts_dir}/{output_dir}/hubble.jpeg'
+
+    
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
 
-filename = f'{scripts_dir}/images/hubble.jpeg'
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+def main():
+    url = ''  # input('Enter picture\'s URL\n')
+    output_dir = input('Enter output path\n')
+    get_space_photo(url, output_dir)
 
-response = requests.get(url)
-response.raise_for_status()
 
-with open(filename, 'wb') as file:
-    file.write(response.content)
+if __name__ == "__main__":
+    main()
