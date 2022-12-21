2c468cbcc323191b60e34ff0794cf60468e6934e
Almaznur1 <aboohnifa@gmail.com>
2022-12-21 16:45:59 +0300

    step7 completed

--- 1.py
+++ 1.py
@@ -1,3 +1,16 @@
-books = ['book1', 'book2', 'book3']
-for book_number, book in enumerate(books):
-    print(book_number, book)
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+token = os.environ["NASA_API_TOKEN"]
+url = 'https://api.nasa.gov/planetary/apod/'
+params = {'api_key': token}
+
+response = requests.get(url, params=params)
+print(response.json()['url'])
+filename = 'picture_of_the_day.jpeg'
+# with open(filename, 'wb') as file:
+#     file.write(response.content)
+#     print(file)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 
 def fetch_spacex_last_launch(save_dir):
@@ -8,9 +9,11 @@ def fetch_spacex_last_launch(save_dir):
         save_dir = f'{os.path.dirname(os.path.abspath(__file__))}/{save_dir}'
     Path(save_dir).mkdir(parents=True, exist_ok=True)
 
-    response = requests.get('https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a')
+    # in the case there is no photos use this flight id: 5eb87d47ffd86e000604b38a
+    response = requests.get('https://api.spacexdata.com/v5/launches/latest')
     response.raise_for_status()
-    urls = response.json()['links']['flickr']['original'] 
+    urls = response.json()['links']['flickr']['original']
+
     for index, url in enumerate(urls):
         response = requests.get(url)
         response.raise_for_status()
@@ -19,9 +22,20 @@ def fetch_spacex_last_launch(save_dir):
             file.write(response.content)
 
 
+def get_nasa_picture_of_the_day(token):
+    url = 'https://api.nasa.gov/planetary/apod/'
+    params = {'api_key': token}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    print(response.json()['url'])
+
+
 def main():
+    load_dotenv()
+    # token = os.environ["NASA_API_TOKEN"]
     output_dir = input('Enter output path\n')
     fetch_spacex_last_launch(output_dir)
+    # get_nasa_picture_of_the_day(token)
 
 
 if __name__ == "__main__":
