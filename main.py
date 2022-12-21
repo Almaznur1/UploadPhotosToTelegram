ff486936bab09032b8399e2e81f902dc4aaaa9b1
Almaznur1 <aboohnifa@gmail.com>
2022-12-21 14:59:11 +0300

    step5 completed

--- main.py
+++ main.py
@@ -3,42 +3,48 @@ from pathlib import Path
 import os
 
 
-def get_space_photo(url, output_dir):
-    # absolute path case
-    if os.path.isabs(output_dir):
-        Path(f'{output_dir}').mkdir(parents=True, exist_ok=True)
-        filename = f'{output_dir}/hubble.jpeg'
-    # relative path case
-    else:
-        scripts_dir = os.path.dirname(os.path.abspath(__file__))
-        Path(f'{scripts_dir}/{output_dir}').mkdir(parents=True, exist_ok=True)
-        filename = f'{scripts_dir}/{output_dir}/hubble.jpeg'
+# def get_hubble_photo(save_dir):
+#     if not os.path.isabs(save_dir):  # relative path case
+#         save_dir = f'{os.path.dirname(os.path.abspath(__file__))}/{save_dir}'
+#     Path(save_dir).mkdir(parents=True, exist_ok=True)
+#     filename = f'{save_dir}/hubble.jpeg'
 
+#     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+#     response = requests.get(url)
+#     response.raise_for_status()
 
-    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    response = requests.get(url)
-    response.raise_for_status()
+#     with open(filename, 'wb') as file:
+#         file.write(response.content)
 
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+def get_spacex_photo(urls, save_dir):
+    if not os.path.isabs(save_dir):  # relative path case
+        save_dir = f'{os.path.dirname(os.path.abspath(__file__))}/{save_dir}'
+    Path(save_dir).mkdir(parents=True, exist_ok=True)
+
+    for index, url in enumerate(urls):
+        response = requests.get(url)
+        response.raise_for_status()
+        filename = f'{save_dir}/spacex_{index}.jpg'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
 
 
 def get_spacex_launch_photo_urs():
     response = requests.get('https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a')
     response.raise_for_status()
-
-    space_launch_photo_urls = response.json()['links']['flickr']['original']
-    for i in range(len(space_launch_photo_urls)):
-        print(space_launch_photo_urls[i])
+    photo_urls = response.json()['links']['flickr']['original'] 
+    return (photo_urls)
 
 
 def main():
-    url = ''  # input('Enter picture\'s URL\n')
+    # Hubble photo getting 
     output_dir = input('Enter output path\n')
-    get_space_photo(url, output_dir)
-    get_spacex_launch_photo_urs()
+    # get_hubble_photo(output_dir)
+
+    # SpaceX launch photo
+    urls = get_spacex_launch_photo_urs()
+    get_spacex_photo(urls, output_dir)
 
 
 if __name__ == "__main__":
     main()
- 
