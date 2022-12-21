a6008e2b69e5feb5c7cf844ec072e0d27fcb2dd7
Almaznur1 <aboohnifa@gmail.com>
2022-12-21 23:53:51 +0300

    step9 completed

--- main.py
+++ main.py
@@ -23,12 +23,25 @@ def fetch_spacex_last_launch(save_dir):
             file.write(response.content)
 
 
-def get_nasa_picture_of_the_day(token):
+def get_nasa_apod(token, save_dir):
+    if not os.path.isabs(save_dir):  # relative path case
+        save_dir = f'{os.path.dirname(os.path.abspath(__file__))}/{save_dir}'
+    Path(save_dir).mkdir(parents=True, exist_ok=True)
+
     url = 'https://api.nasa.gov/planetary/apod/'
-    params = {'api_key': token}
+    params = {'api_key': token, 'start_date': '2022-11-01', 'end_date': ''}
     response = requests.get(url, params=params)
     response.raise_for_status()
-    print(response.json()['url'])
+    urls = []
+    for dict in response.json():
+        urls.append(dict['url'])
+    
+    for index, url in enumerate(urls):
+        response = requests.get(url)
+        response.raise_for_status()
+        filename = f'{save_dir}/nasa_apod_{index}.jpg'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
 
 
 def get_file_ext(url):
@@ -40,10 +53,9 @@ def get_file_ext(url):
 def main():
     load_dotenv()
     token = os.environ["NASA_API_TOKEN"]
-
-    # output_dir = input('Enter output path\n')
+    output_dir = input('Enter output path\n')
     # fetch_spacex_last_launch(output_dir)
-    # get_nasa_picture_of_the_day(token)
+    get_nasa_apod(token, output_dir)
     # url = 'https://example.com/txt/hello%20world.txt?v=9#python'
     # print(get_file_ext(url))
 
