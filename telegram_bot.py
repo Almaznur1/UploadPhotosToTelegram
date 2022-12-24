7894f57609774e1a2a9a6335ba9624fac738006e
Almaznur1 <aboohnifa@gmail.com>
2022-12-24 14:19:40 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,6 +6,5 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.environ['TELEGRAM_BOT_TOKEN']
 bot = telegram.Bot(token=token)
-print(bot.get_me())
-bot.send_message(chat_id='@AlmazNuriakhmetov', text="I'm sorry Dave I'm afraid I can't do that.")
-# bot.send_message(chat_id='-1001874718165', text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_document(chat_id='-1001874718165', document=open('images/spacex_1.jpg', 'rb'))
+bot.send_message(chat_id='-1001874718165', text="I'm sorry Dave I'm afraid I can't do that.")
